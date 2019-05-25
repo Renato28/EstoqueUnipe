f46c33545529afb35f866d5db585e0f9faba5f70
= <=>
2019-05-25 15:34:33 -0300

    projeto adicionado

--- src/main/java/br/com/unipe/entidade/Endereco.java
+++ src/main/java/br/com/unipe/entidade/Endereco.java
@@ -7,30 +7,30 @@ import org.hibernate.validator.constraints.NotEmpty;
 import br.com.unipe.enumerator.Cidades;
 import br.com.unipe.enumerator.Estados;
 
+public class Endereco implements Serializable {
 
-public class Endereco implements Serializable{
-	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
-	
 	@NotEmpty
 	private Cidades municipios;
-	
+
 	@NotEmpty
 	private Estados estados;
-	
+
 	@NotEmpty
 	private String bairro;
 
 	@NotEmpty
 	private Integer numero;
-	
+
 	@NotEmpty
 	private String cep;
 
+	@NotEmpty
+	private String logradouros;
 
 	public Cidades getMunicipios() {
 		return municipios;
@@ -47,7 +47,7 @@ public class Endereco implements Serializable{
 	public void setEstados(Estados estados) {
 		this.estados = estados;
 	}
-	
+
 	public String getBairro() {
 		return bairro;
 	}
@@ -70,6 +70,14 @@ public class Endereco implements Serializable{
 
 	public void setCep(String cep) {
 		this.cep = cep;
+	}
+
+	public String getLogradouros() {
+		return logradouros;
+	}
+
+	public void setLogradouros(String logradouros) {
+		this.logradouros = logradouros;
 	}
 
 }
