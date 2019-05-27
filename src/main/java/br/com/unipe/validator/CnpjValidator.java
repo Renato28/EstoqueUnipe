ea78fac34e16c7a132d7e9da2a595eebe51c7530
= <=>
2019-05-27 00:36:47 -0300

    projeto atualizado

---
+++ src/main/java/br/com/unipe/validator/CnpjValidator.java
@@ -0,0 +1,23 @@
+package br.com.unipe.validator;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+@FacesValidator("cpfValidator")
+public class CnpjValidator implements Validator {
+
+	@Override
+	public void validate(FacesContext arg0, UIComponent arg1, Object arg2) throws ValidatorException {
+		String cnpj = arg2.toString();
+		if (cnpj == null || (cnpj != null && (cnpj.equals("") || cnpj.length() < 14))) {
+			FacesMessage fm = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Cnpj menor que 14 Caracteres",
+					"Cnpj menor que 14 Caracteres");
+			throw new ValidatorException(fm);
+		}
+	}
+
+}
